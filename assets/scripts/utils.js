038d4b7b641b5b59af7a0399dee5d7cc1414d0b2
Sofistication <sordonez93@gmail.com>
2017-02-17 15:40:31 -0500

    Implement basic auth actions
    - added modals for auth actions
    - tested auth interaction with back end
    - forever ugly front end

---
+++ assets/scripts/utils.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const clearInput = function (element) {
+  $(element + ' > div > input').val('');
+};
+
+const clearModalInput = function (element) {
+  $(element + ' > fieldset > label > input').val('');
+};
+
+const addErrorMessage = function (element, status) {
+  let html = '<p class="modal-error">';
+  switch (status) {
+    case 401:
+      html += 'Wrong Password!';
+      break;
+    default:
+      html += 'Unexpected Error!';
+  }
+  html += '</p>';
+  $(element).append(html);
+};
+
+const mainDisplay = function () {
+  $('.second-bar').removeClass('hidden');
+  $('.second-bar').show();
+  $('.initial-bar').hide();
+};
+
+const openingDisplay = function () {
+  $('.second-bar').hide();
+  $('.initial-bar').show();
+};
+
+module.exports = {
+  clearInput,
+  clearModalInput,
+  addErrorMessage,
+  mainDisplay,
+  openingDisplay
+};
